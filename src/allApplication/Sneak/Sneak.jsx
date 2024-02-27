36c8994df66b51fa2027680ca586fc3e4d73aa90
brahim <brahim@gmail.com>
2024-02-27 20:13:53 +0100

    face 1

--- src/allApplication/Sneak/Sneak.jsx
+++ src/allApplication/Sneak/Sneak.jsx
@@ -5,12 +5,15 @@ const Sneak = () => {
   const [arrayStart, setArrayStart] = useState([1, 2, 3, 4, 5, 6]); // lenght 6 , indexMax 5
   const theSneak = useRef();
 
+  const [testKey, setKey] = useState(10);
   const [arrayLengthChange, setArrayLength] = useState(arrayStart.length);
   // setToggleKeys();
   const [keysToggle, setToggleKeys] = useState();
   // idIntervals ();
   const [idDown, setIdDown] = useState();
   const [idRight, setIdRight] = useState();
+  const [idLeft, setIdLeft] = useState();
+  const [idUpp, setIdUpp] = useState();
   const [arrayCordinationMainSneak, setMainCordination] = useState();
   // this 2 array is for the functions
   const [arrayCordination, setArrayCordination] = useState();
@@ -64,127 +67,280 @@ const Sneak = () => {
   };
 
   // 1/ move to Left for the Sneak Div :
-  const moveToLeft = (elem) => {
+  const moveToLeft = (elem, index, arraySpans) => {
     const cordination = elem.getBoundingClientRect();
-    elem.style.transform = `translateX(${cordination.x + 40}px)`;
+    console.log("Hello from map moveToLeft , : ", cordination.top, index);
+
+    //  from down
+
+    if (cordination.bottom < arraySpans[0].getBoundingClientRect().bottom) {
+      // move 16 px to right
+      elem.style.top = `${cordination.y + 16}px `;
+      console.log("Yesssp ", index);
+    }
+
+    if (testKey == 1) {
+      if (cordination.bottom > arraySpans[0].getBoundingClientRect().bottom) {
+        // move 16 px to right
+        elem.style.top = `${cordination.y - 16}px `;
+        console.log("Yesssp ", index);
+      }
+    }
+
+    // origin move
+    if (
+      cordination.left - 16 >= 0 &&
+      cordination.top == arraySpans[0].getBoundingClientRect().top
+    ) {
+      // move 16px to bottom
+      elem.style.left = `${cordination.x - 16}px `;
+      console.log("yyyyyyyyy ", index);
+    }
+
+    // the limites of shutDown the operation
+    if (cordination.left - 16 <= 0 && index == arraySpans.length - 1) {
+      setToggleKeys(5);
+    }
   };
 
   // 2/ move to Right for the Sneak Div :
-  const moveToRight = (elem) => {
-    // console.log("hello from the moveToRight ")
-    // setMainCordination(theSneak.current.getBoundingClientRect());
-    // const newId = setInterval(() => {
-    //   arraySpans.map((elem, index, arraySpans) => {
-    //     const cordination = elem.getBoundingClientRect();
-    //     elem.style.background = "red";
-    //     //   condition to move to right
-    //     if (cordination.right < window.innerWidth) {
-    //       // move 16 px to right
-    //       elem.style.left = `${cordination.x + 16}px `;
-    //       // console.log("Yesssp ", index);
-    //     }
-    //     // condition of shutDown
-    //     if (cordination.right + 16 + 16 * index >= window.innerWidth) {
-    //       console.log("this is the toggle 5 ", index);
-    //       setToggleKeys(5);
-    //     }
-    //   });
-    // }, 500);
-    // setIdRight(newId);
+  const moveToRight = (elem, index, arraySpans) => {
+    const cordination = elem.getBoundingClientRect();
+    setTimeout(() => {
+      elem.style.background = "red";
+    }, 500 * index);
+    //   condition to move to right
+
+    if (testKey == 2) {
+      if (cordination.bottom < arraySpans[0].getBoundingClientRect().bottom) {
+        // move 16 px to right
+        elem.style.top = `${cordination.y + 16}px `;
+        console.log("Yesssp ", testKey, index);
+      }
+    }
+
+    // case from upp into right
+
+    if (testKey == 1) {
+      if (cordination.bottom > arraySpans[0].getBoundingClientRect().bottom) {
+        console.log("Yesssp ", testKey, index);
+        elem.style.top = `${cordination.y - 16}px `;
+      }
+    }
+
+    console.log("______________************* _____ Right key  :");
+
+    if (
+      cordination.right < window.innerWidth &&
+      cordination.top == arraySpans[0].getBoundingClientRect().top
+    ) {
+      elem.style.left = `${cordination.x + 16}px `;
+      // console.log("Yesssp ", index);
+    }
+    // condition of shutDown
+    if (
+      cordination.right + 16 >= window.innerWidth &&
+      index == arraySpans.length - 1
+    ) {
+      console.log("this is the toggle 5 ", index);
+      setToggleKeys(5);
+    }
   };
 
-  // 3/ move to down for the Sneak Div :
-  const moveToDown = () => {
-    const newId = setInterval(() => {
-      arraySpans.map((elem, index, arraySpans) => {
-        updateCordination();
-        console.log(arrayCordination);
-        const cordination = elem.getBoundingClientRect();
-        elem.style.background = "red";
-        //   cordination.right < arraySpans[0].getBoundingClientRect().right
-        if (cordination.right < arraySpans[0].getBoundingClientRect().right) {
-          // move 16 px to right
-          elem.style.left = `${cordination.x + 16}px `;
-          // console.log("Yesssp ", index);
-        } else {
-          console.log("NNNNNNNNN", index);
-          // setToggleKeys(5);
-        }
-        if (
-          cordination.bottom + 16 + 16 * index <= window.innerHeight &&
-          Math.floor(cordination.right) ==
-            Math.floor(arraySpans[0].getBoundingClientRect().right)
-        ) {
-          // move 16px to bottom
-          elem.style.top = `${cordination.y + 16}px `;
-        }
+  // update the coordination for the first rendering :
+  useEffect(() => {
+    updateCordination();
+  }, []);
+  // useEffect(() => {
+  //   console.log("_____________________");
+  //   console.log(arrayCordination);
+  //   console.log("_____________________");
+  // }, [arrayCordination]);
 
-        if (cordination.bottom + 16 >= window.innerHeight) {
-          setToggleKeys(5);
-        }
-      });
-    }, 500);
-    setIdDown(newId);
+  // 3/ move to down for the Sneak Div :
+  const moveToDown = (elem, index, arraySpans) => {
+    const cordination = elem.getBoundingClientRect();
+    setTimeout(() => {
+      elem.style.background = "red";
+    }, 500 * index);
+    //   cordination.right < arraySpans[0].getBoundingClientRect().right
+
+    if (testKey != 3) {
+      if (cordination.right < arraySpans[0].getBoundingClientRect().right) {
+        // move 16 px to right
+        console.log("________________________ Down Key  :", testKey);
+        elem.style.left = `${cordination.x + 16}px `;
+        // console.log("Yesssp ", index);
+      }
+    }
+    if (testKey == 3) {
+      if (cordination.right > arraySpans[0].getBoundingClientRect().right) {
+        elem.style.left = `${cordination.x - 16}px `;
+      }
+    }
+    if (
+      cordination.bottom + 16 <= window.innerHeight &&
+      Math.floor(cordination.right) ==
+        Math.floor(arraySpans[0].getBoundingClientRect().right)
+    ) {
+      // move 16px to bottom
+      elem.style.top = `${cordination.y + 16}px `;
+    }
+
+    if (
+      cordination.bottom + 16 >= window.innerHeight &&
+      index == arraySpans.length - 1
+    ) {
+      setToggleKeys(5);
+    }
   };
 
   // 4/ move to up for the Sneak Div :
-  const moveToUpp = (elem) => {
+  const moveToUpp = (elem, index, arraySpans) => {
     const cordination = elem.getBoundingClientRect();
-    elem.style.transform = `translateX(${cordination.x + 40}px)`;
+    setTimeout(() => {
+      elem.style.background = "red";
+    }, 500 * index);
+    //   cordination.right < arraySpans[0].getBoundingClientRect().right
+    if (testKey != 3) {
+      if (cordination.left < arraySpans[0].getBoundingClientRect().left) {
+        // move 16 px to right
+        elem.style.left = `${cordination.x + 16}px `;
+        // console.log("Yesssp ", index);
+      }
+    }
+
+    if (testKey == 3) {
+      if (cordination.right > arraySpans[0].getBoundingClientRect().right) {
+        elem.style.left = `${cordination.x - 16}px `;
+      }
+    }
+    // origin move :
+    if (
+      cordination.top - 16 >= 0 &&
+      Math.floor(cordination.left) ==
+        Math.floor(arraySpans[0].getBoundingClientRect().left)
+    ) {
+      // move 16px to bottom
+      elem.style.top = `${cordination.y - 16}px `;
+    }
+    // condition shutdown
+
+    if (cordination.top - 16 <= 0 && index == arraySpans.length - 1) {
+      setToggleKeys(5);
+    }
   };
 
+  // function help of loop intervals
+
+  const clearFunction = (table) => {
+    for (let i = 0; i < table.length; i++) {
+      setTimeout(() => {
+        clearInterval(table[i]);
+      }, 500 * i);
+    }
+  };
   // the main controol
   useEffect(() => {
     // console the array
     if (keysToggle == 1) {
-      // moveToUpp();
-      // return clearInterval(idDown);
+      clearInterval(idRight);
+      clearInterval(idLeft);
+      clearInterval(idDown);
+
+      setKey(1);
+
+      const newId = setInterval(() => {
+        arraySpans.map((elem, index, arraySpans) => {
+          moveToUpp(elem, index, arraySpans);
+        });
+      }, 300);
+
+      setIdUpp(newId);
     }
     if (keysToggle == 2) {
-      // updateCordination();
-      // clearInterval(idRight);
-      console.log(arrayCordination);
+      clearInterval(idRight);
+      clearInterval(idLeft);
+      clearInterval(idUpp);
+      setKey(2);
 
-      moveToDown();
-      return () => {
-        clearInterval(idDown);
-      };
+      const newId = setInterval(() => {
+        arraySpans.map((elem, index, arraySpans) => {
+          moveToDown(elem, index, arraySpans);
+        });
+      }, 300);
+      setIdDown(newId);
     }
     if (keysToggle == 3) {
-      // moveToLeft();
       clearInterval(idDown);
+      clearInterval(idRight);
+      clearInterval(idUpp);
+
+      setKey(3);
+
+      const newId = setInterval(() => {
+        arraySpans.map((elem, index, arraySpans) => {
+          moveToLeft(elem, index, arraySpans);
+        });
+      }, 300);
+
+      setIdLeft(newId);
       // console.log(arrayCordination);
     }
     if (keysToggle == 4) {
+      clearInterval(idDown);
+      clearInterval(idLeft);
+      clearInterval(idUpp);
+
+      setKey(4);
+
+      const newId = setInterval(() => {
+        arraySpans.map((elem, index, arraySpans) => {
+          moveToRight(elem, index, arraySpans);
+        });
+      }, 300);
+
+      setIdRight(newId);
     }
 
     if (keysToggle === 5) {
-      console.log("&&&&&&&&&&&&&&&&&&&&_______________--");
-      console.log(arrayCordination);
       clearInterval(idDown);
+      clearInterval(idRight);
+      clearInterval(idUpp);
+      clearInterval(idLeft);
       // clearInterval(idRight);
     }
-  }, [keysToggle, arrayCordination]);
+  }, [keysToggle]);
 
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowUp") {
-      // Perform action for arrow up key press
-      setToggleKeys(1);
-      console.log("Arrow Up pressed");
-    } else if (event.key === "ArrowDown") {
-      // Perform action for arrow down key press
-      console.log("Arrow Down pressed");
-      setToggleKeys(2);
-    } else if (event.key === "ArrowLeft") {
-      // Perform action for arrow left key press
-      setToggleKeys(3);
-      console.log("Arrow Left pressed");
-    } else if (event.key === "ArrowRight") {
-      setToggleKeys(4);
-      // Perform action for arrow right key press
-      console.log("Arrow Right pressed");
+  const handleArrowKeyPress = (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        setToggleKeys(1);
+        console.log("Arrow Up pressed");
+        break;
+      case "ArrowDown":
+        setToggleKeys(2);
+        console.log("Arrow Down pressed");
+        break;
+      case "ArrowLeft":
+        setToggleKeys(3);
+        console.log("Arrow Left pressed");
+        break;
+      case "ArrowRight":
+        setToggleKeys(4);
+        console.log("Arrow Right pressed");
+        break;
+      default:
+        break;
     }
-  });
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleArrowKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleArrowKeyPress);
+    };
+  }, []);
 
   return (
     <>
