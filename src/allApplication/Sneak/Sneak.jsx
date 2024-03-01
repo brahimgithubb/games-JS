f5cf942dd5502a2ffaad31485b972f4b70cfb3a3
brahim <brahim@gmail.com>
2024-03-01 12:14:47 +0100

    new View with other issue

--- src/allApplication/Sneak/Sneak.jsx
+++ src/allApplication/Sneak/Sneak.jsx
@@ -8,8 +8,12 @@ const Sneak = () => {
   // keyMove from 1 to 5 (5 is for shutDown the intervels and clear all intervels )
   const [keyMove, setKeyMove] = useState();
 
+  // previous key :
+  const [previousKey, setPreviousKey] = useState(10);
+
   // All Intervl ids
-  const [idRight, setIdRight] = useState();
+  const [idRight, setIdRight] = useState([]);
+  const [idDown, setIdDown] = useState([]);
 
   // get update coordination of the spanArray :
   const updateSneak = () => {
@@ -58,14 +62,39 @@ const Sneak = () => {
   }, []);
   // done between span 16px
 
-  // start  function Right :
-  const moveToRight = (elem, index, coordinationElem) => {
-    if (coordinationElem.right + 16 * index < window.innerWidth) {
+  //2// start of the moveToDown()
+
+  const moveToDown = (elem, index, coordinationElem, firstSpan) => {
+    if (coordinationElem.right < firstSpan.right) {
+      elem.style.left = `${coordinationElem.left + 16}px`;
+    }
+    if (
+      coordinationElem.bottom + 16 * index < window.innerHeight &&
+      coordinationElem.left == firstSpan.left
+    ) {
+      elem.style.top = `${coordinationElem.top + 16}px`;
+    }
+    if (coordinationElem.bottom + 16 >= window.innerHeight) {
+      setKeyMove(5);
+    }
+    console.log("Hello from outside ");
+  };
+
+  //2// end of the moveToDown()
+
+  // 4 // start  function Right :
+  const moveToRight = (elem, index, coordinationElem, firstSpan) => {
+    if (
+      coordinationElem.right + 16 * index < window.innerWidth &&
+      coordinationElem.top == firstSpan.top
+    ) {
       console.log(
-        "coordination left  of first span red : ",
-        coordinationElem.x,
-        "this is the width of the page ",
-        window.innerWidth
+        "top of firstSpan : ",
+        firstSpan.top,
+        "top of elem : ",
+        coordinationElem.top,
+        "index is : ",
+        index
       );
       elem.style.left = `${coordinationElem.left + 16}px`;
     }
@@ -75,6 +104,9 @@ const Sneak = () => {
     }
     console.log("Hello from outside ");
   };
+
+  // 4 //end of the moveToRight()
+
   const handleArrowKeyPress = (event) => {
     switch (event.key) {
       case "ArrowUp":
@@ -107,28 +139,85 @@ const Sneak = () => {
     };
   }, []);
 
-  // useEffect base on the keyMove
+  //  end of the key Event
 
+  // clearFunction :
+
+  const clearFunction = (table) => {
+    table.forEach((elem, index) => {
+      clearInterval(elem);
+    });
+  };
+  const clearFunctionDelay = (table) => {
+    table.forEach((elem, index) => {
+      setTimeout(() => {
+        clearInterval(elem);
+      }, 500 * index);
+    });
+  };
+
+  // end Clear Function :
+
+  // useEffect base on the keyMove
   useEffect(() => {
     switch (keyMove) {
       case 1:
         break;
       case 2:
-        break;
+        clearFunction(idRight);
+        setPreviousKey(2);
+        const arraySpanDown = updateSneak();
+        const fisrtSpan = updateCoordination()[0];
+        const idDownNew = arraySpanDown.map((elem, index) => {
+          return setInterval(() => {
+            // const arrayCoordination = updateCoordination();
+
+            setTimeout(
+              () => {
+                moveToDown(
+                  elem,
+                  index,
+                  arraySpanDown[index].getBoundingClientRect(),
+                  fisrtSpan
+                );
+              },
+              previousKey != 10 && index != 0 ? 500 * index : 0
+            );
+          }, 500);
+        });
+        setIdDown([...idDown, ...idDownNew]);
+        return () => clearFunction(idDown);
       case 3:
         break;
       case 4:
-        const intervalId = setInterval(() => {
-          const arraySpan = updateSneak();
-          // const arrayCoordination = updateCoordination();
-          arraySpan.forEach((elem, index) => {
-            moveToRight(elem, index, arraySpan[index].getBoundingClientRect());
-          });
-        }, 200);
-        setIdRight(intervalId);
-        return () => clearInterval(idRight);
+        clearFunctionDelay(idDown);
+        setPreviousKey(4);
+        const arraySpan = updateSneak();
+        const fisrtSpanRight = updateCoordination()[0];
+
+        const idRightNew = arraySpan.map((elem, index) => {
+          return setInterval(() => {
+            // const arrayCoordination = updateCoordination();
+
+            setTimeout(
+              () => {
+                moveToRight(
+                  elem,
+                  index,
+                  arraySpan[index].getBoundingClientRect(),
+                  // arraySpan[0].getBoundingClientRect()
+                  fisrtSpanRight
+                );
+              },
+              previousKey != 10 && index != 0 ? 500 * index : 0
+            );
+          }, 500);
+        });
+        setIdRight([...idRight, ...idRightNew]);
+        return () => clearFunction(idRight);
       case 5:
-        clearInterval(idRight);
+        clearFunction(idRight);
+        clearFunction(idDown);
         break;
     }
   }, [keyMove]);
